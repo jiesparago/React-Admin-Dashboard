e09fb5524c35f273710b585967b7f18953e26547
Sep <esparago.joseph0002@gmail.com>
2023-02-04 20:30:49 +0800

    Add contacts

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -16,7 +16,7 @@ import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutl
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
-import { fontWeight } from '@mui/system';
+
 
 const Item = ({ title, to, icon, selected, setSelected }) => {
   const theme = useTheme();
